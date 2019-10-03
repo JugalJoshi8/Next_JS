ecc6da8bd2cff39e414c7c05782be69c69283717
Jugal Joshi <jugjoshi@publicisgroupe.net>
2019-10-03 16:56:40 +0530

    added header in layout

--- components/hoc/withLayout.js
+++ components/hoc/withLayout.js
@@ -1,4 +1,5 @@
 import Header from '../Header';
+import Head from 'next/head'
 const layoutStyle = {
   margin: 20,
   padding: 20,
@@ -8,6 +9,14 @@ const withLayout = Page => {
   return props =>  {
     return (
     <div style = {layoutStyle}>
+      <Head>
+        <title>My page title</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+      </Head>
       <Header></Header>
       <Page {...props}></Page>
     </div>
