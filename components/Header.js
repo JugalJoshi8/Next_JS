a266881972e27aba3441f4886c3a40cdea9bf2c5
Jugal <jkj.619@gmail.com>
2019-11-20 10:41:17 +0530

    UI changes

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,13 @@ function Header() {
         <a style = {linkStyle} title="home">Home Page</a>
       </Link>
       <Link href="/about">
-        <a title="About">About Page</a>
+        <a style = {linkStyle} title="About">About Page</a>
+      </Link>
+      <Link href="/blog">
+        <a style = {linkStyle} title="Blog">Blog Page</a>
+      </Link>
+      <Link href="/tvshows">
+        <a style = {linkStyle} title="TV shows">TV shows Page</a>
       </Link>
     </>
   );
