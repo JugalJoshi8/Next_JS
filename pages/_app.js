99777edebfa1afd188ad264047f31ec4eee0c3f4
Jugal <jkj.619@gmail.com>
2019-11-20 12:15:42 +0530

    created theme object

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
-import App from 'next/app';
-import React from 'react';
+import App from "next/app";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 
-
+const theme = {
+  color: "white",
+  background: "black"
+};
 class MyApp extends App {
-    render() {
-        const { Component, pageProps } = this.props;
-        return (
-            <ThemeProvider theme = {{color: 'white', background: 'black'}}>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        )
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default MyApp;
